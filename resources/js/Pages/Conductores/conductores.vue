7ad797f522c0de61493af99e0b60d0a37181a3c7
jared salazar loera <salazarloerajared@gmail.com>
2024-02-15 21:31:27 -0600

    pruebasServidor

--- resources/js/Pages/Conductores/conductores.vue
+++ resources/js/Pages/Conductores/conductores.vue
@@ -1,30 +1,20 @@
-<script>
+<script setup>
 import tablaConductores from '@/Components/componentesConductor/tablaConductores.vue'
 import cardCountAll from '@/Components/componentesConductor/cardCountAll.vue'
 import cardCountActive from '@/Components/componentesConductor/cardCountActive.vue'
 import cardCountInactive from '@/Components/componentesConductor/cardCountInactive.vue'
-
+</script>
+<script>
 export default {
   props: {
     driversResponse: Object,
     errors: Object,
   },
-  components: {
-    tablaConductores,
-    cardCountAll,
-    cardCountActive,
-    cardCountInactive,
-  }
   
 }
 </script>
 <template>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
+
     <div class="container mx-auto px-4">
       <div class="flex">
         <div class="text-3xl font-bold">
@@ -32,10 +22,10 @@ export default {
         </div>
       </div>
       <div class="flex">
-        <div class="w-1/2">
-          <tablaConductores :drivers="driversResponse.drivers"></tablaConductores>
+        <div class="w-1/2 sm:w-1/1">
+          <tablaConductores :drivers="driversResponse.drivers" />
         </div>
-        <div class="w-1/2">
+        <div class="w-1/2 sm:w-1/1">
           <div class="container mx-auto">
             <div class="flex">
               <div class="text-3xl font-bold">
@@ -43,16 +33,15 @@ export default {
               </div>
             </div>
             <div class="flex flex-wrap">
-              <cardCountAll :totalDrivers="driversResponse.totalDrivers" :errors="errors"></cardCountAll>
-              <cardCountActive :activeDrivers="driversResponse.activeDrivers"></cardCountActive>
-              <cardCountInactive :inactiveDrivers="driversResponse.inactiveDrivers"></cardCountInactive>
+              <cardCountAll :totalDrivers="driversResponse.totalDrivers" :errors="errors"/>
+              <cardCountActive :activeDrivers="driversResponse.activeDrivers" />
+              <cardCountInactive :inactiveDrivers="driversResponse.inactiveDrivers" />
             </div>
           </div>
         </div>
       </div>
     </div>
-  </body>
-  </html>
+
 </template>
 
   
